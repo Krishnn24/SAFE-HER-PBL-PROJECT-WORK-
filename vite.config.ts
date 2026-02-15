87081e9a3ee92a59a8ded5033074a6bec93e7576
Krishnn Vats <krishnnvats24@gmail.com>
2026-02-15 20:49:22 +0530

    changes made for the new version 0.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
  
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({ 
-   base: "/SAFE-HER-PBL-PROJECT-WORK-/",
+ 
   server: { 
    
   host: "localhost",
