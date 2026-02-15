564fe95ba498d4f94bfd544f42f4aec884ebe877
Krishnn Vats <krishnnvats24@gmail.com>
2026-02-15 20:34:03 +0530

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+ 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
+export default defineConfig(({ mode }) => ({ 
+   base: "/SAFE-HER-PBL-PROJECT-WORK-/",
+  server: { 
+   
   host: "localhost",
   port: 8081,
   strictPort: true,
